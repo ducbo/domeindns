3b5f65bb042461b9d6790efcfe0c4e801bc7aa44
Tran Dinh Duc <ducbo@yahoo.com>
2020-05-02 18:39:39 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'DomeinDNS') }}</title>
+    <title>{{ config('app.name', 'PDNS') }}</title>
 
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
