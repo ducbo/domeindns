5838aabcbf0ebff3668ca3b06399d576673eb0fe
Tran Dinh Duc <ducbo@yahoo.com>
2020-05-02 18:40:34 +0700

    Update privacy.blade.php

--- resources/views/legal/privacy.blade.php
+++ resources/views/legal/privacy.blade.php
@@ -7,11 +7,11 @@
                         <p>Effective date: May 16, 2019</p>
 
 
-                        <p>Schneiders IT ("us", "we", or "our") operates the https://domeindns.nl website (the "Service").</p>
+                        <p>Schneiders IT ("us", "we", or "our") operates the https://pdns.win website (the "Service").</p>
 
                         <p>This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data.</p>
 
-                        <p>We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible from https://domeindns.nl</p>
+                        <p>We use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible from https://pdns.win</p>
 
 
                         <h2>Information Collection And Use</h2>
@@ -105,7 +105,7 @@
                         <h2>Contact Us</h2>
                         <p>If you have any questions about this Privacy Policy, please contact us:</p>
                         <ul>
-                            <li>By email: info@domeindns.nl</li>
+                            <li>By email: info@pdns.win</li>
 
                         </ul>
                     </div>
